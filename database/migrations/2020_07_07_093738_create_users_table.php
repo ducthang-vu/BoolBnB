eed95672267df43b2443c7f5640921a27fcd3811
Duc Thang Vu <vuducthang91@gmail.com>
2020-07-07 14:36:21 +0200

    closes #3

--- database/migrations/2020_07_07_093738_create_users_table.php
+++ database/migrations/2020_07_07_093738_create_users_table.php
@@ -15,10 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('surname');
+            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('name');
             $table->date('date_of_birth');
             $table->rememberToken();
             $table->timestamps();
