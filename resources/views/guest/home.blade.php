ef2c850223154fc1f9ff36bdb66551bf579fac35
Nicola <niconix.dev@gmail.com>
2020-07-09 12:42:12 +0200

    closes #32

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -2,14 +2,37 @@
 
 
 @section('page-content')
+
+<div class="search-home d-flex">
     <form action="#" method="GET">
         @csrf
         @method('GET')
         <input type="text" name="address" id="address" placeholder="Cerca un appartamento">
         <input type="hidden" name="latlong" id="latlong">
-        <input type="submit" value="Cerca">
+        <input type="submit" value="Cerca" class="button-home">
     </form>
+</div>
 
-    @dump($sponsoredFlats)
+<h1 class="title-home">Appartamenti sponsorizzati</h1>
+
+<div class="sponsorship-card d-flex">
+    @foreach($sponsoredFlats as $sponsoredFlat)
+    <a class="card m-10" href="">
+
+    <img src="{{$sponsoredFlat->image}}" alt="{{$sponsoredFlat->title}}">
+
+    <h3>{{$sponsoredFlat->title}}</h3>
+
+    </a>
+
+    @if ($loop->index == 5)
+        @break;
+    
+    @endif
+    @endforeach
+</div>
+    
+
+
 
 @endsection
