2d4ecb2d6c853a1acc0a5ecc956b02ed9816e872
Dante L'Erario <dantelerario@gmail.com>
2020-07-09 12:42:22 +0200

    fixed layout

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.app')
+@extends('layouts.main')
 
-@section('content')
+@section('page-content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
