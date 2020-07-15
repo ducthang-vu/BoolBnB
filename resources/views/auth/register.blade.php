989b321d3c6a3ee94df5276382054613ce123e81
Dante L'Erario <dantelerario@gmail.com>
2020-07-15 12:44:23 +0200

    closes #56 fixes/3

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,7 +14,7 @@
                     @csrf
 
                     <div class="form-group row">
-                        <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label>
+                        {{-- <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label> --}}
 
                         <div class="col-md-6">
                             <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" placeholder="NAME"autofocus>
@@ -28,7 +28,7 @@
                     </div>
 
                     <div class="form-group row">
-                        <label for="surname" class="col-md-4 col-form-label text-md-right">{{ __('Cognome') }}</label>
+                        {{-- <label for="surname" class="col-md-4 col-form-label text-md-right">{{ __('Cognome') }}</label> --}}
 
                         <div class="col-md-6">
                             <input id="surname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" required autocomplete="surname" placeholder="SURNAME" autofocus>
@@ -42,10 +42,10 @@
                     </div>
 
                     <div class="form-group row">
-                        <label for="date_of_birth" class="col-md-4 col-form-label text-md-right">{{ __('Data di nascita') }}</label>
+                        {{-- <label for="date_of_birth" class="col-md-4 col-form-label text-md-right">{{ __('Data di nascita') }}</label> --}}
 
                         <Div class="col-md-6">
-                            <input id="date_of_birth" type="text" class="form-control @error('date_of_birth') is-invalid @enderror" name="date_of_birth" value="{{ old('date_of_birth') }}" required autocomplete="date_of_birth" placeholder="YYYY-MM-DD" autofocus>
+                            <input id="date_of_birth" type="text" class="form-control @error('date_of_birth') is-invalid @enderror" name="date_of_birth" value="{{ old('date_of_birth') }}" required autocomplete="date_of_birth" placeholder="DATE OF BIRTHDAY YYYY-MM-DD" autofocus>
 
                             @error('date_of_birth')
                                 <span class="invalid-feedback" role="alert">
@@ -56,7 +56,7 @@
                     </div>
 
                     <div class="form-group row">
-                        <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
+                        {{-- <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label> --}}
 
                         <div class="col-md-6">
                             <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="E-MAIL" required autocomplete="email">
@@ -70,7 +70,7 @@
                     </div>
 
                     <div class="form-group row">
-                        <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                        {{-- <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label> --}}
 
                         <div class="col-md-6">
                             <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="PASSWORD">
@@ -84,7 +84,7 @@
                     </div>
 
                     <div class="form-group row">
-                        <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Conferma Password') }}</label>
+                        {{-- <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Conferma Password') }}</label> --}}
 
                         <div class="col-md-6">
                             <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="CONFIRM PASSWORD">
