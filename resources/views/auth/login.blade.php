0518f27659f261fde31f92f7a5809e60347b7fae
Dante L'Erario <dantelerario@gmail.com>
2020-07-16 11:49:11 +0200

    importati file da dev3 su dev

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,10 +2,10 @@
 
 @section('page-content') --}}
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+            <div class="card-popup">
+                <div class="card-header">{{ __('Login') }}
+                    <span onclick="closePopUpLogin()" class="close popup-animation"><i class="far fa-times-circle"></i></span>
+                </div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
