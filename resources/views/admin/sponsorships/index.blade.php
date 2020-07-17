271617aaa2d5e49c92e45632f64c11e7df969e3a
Dante L'Erario <dantelerario@gmail.com>
2020-07-17 17:47:26 +0200

    sponsorship page fix, added empty check

--- resources/views/admin/sponsorships/index.blade.php
+++ resources/views/admin/sponsorships/index.blade.php
@@ -16,38 +16,44 @@
                 <strong>Payment made with id: #{{ session('transactionId') }} </strong>
             </div>
         @endif
-        <div class="container p-25">
-            <a class="btn-sponsorship-index-page" href="{{ route('admin.home') }}">Go back to flat list</a>
-            <table class="table text-center">
-                <thead>
-                    <tr class="head-row">
-                        <th scope="col">#</th>
-                        <th scope="col">Flat #</th>
-                        <th scope="col">Sponsorship</th>
-                        <th scope="col">Start</th>
-                        <th scope="col">End</th>
-                        <th scope="col">Price paid</th>
-                        <th scope="col">Payment code</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($sponsorships as $sponsorship)
-                        <tr>
-                            <td scope="row">{{ $sponsorship->id }}</th>
-                            <td>{{ $sponsorship->flat_id }}</td>
-                            <td>{{ $sponsorship->sponsor_type }}</th>
-                            <td>{{ Carbon\Carbon::parse($sponsorship->start)->format('d/m/Y h:m') }}</td>
-                            <td>{{ Carbon\Carbon::parse($sponsorship->end)->format('d/m/Y h:m') }}</td>
-                            <td>{{ $sponsorship->price }}</td>
-                            <td>{{ $sponsorship->braintree_code }}</td>
+        @if ($sponsorships->count())
+            <div class="container p-25">
+                <a class="btn-sponsorship-index-page" href="{{ route('admin.home') }}">Go back to flat list</a>
+                <table class="table text-center">
+                    <thead>
+                        <tr class="head-row">
+                            <th scope="col">#</th>
+                            <th scope="col">Flat #</th>
+                            <th scope="col">Sponsorship</th>
+                            <th scope="col">Start</th>
+                            <th scope="col">End</th>
+                            <th scope="col">Price paid</th>
+                            <th scope="col">Payment code</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
-            <div class="active-legend-div-indexSponsorship-page">
-                <div class="greendiv-indexSponsorship-page"><i class="fas fa-circle"></i></div>
-                <span class="span-indexSponsorship-page">= la sponsorizzazione è attiva</span>
+                    </thead>
+                    <tbody>
+                        @foreach($sponsorships as $sponsorship)
+                            <tr>
+                                <td scope="row">{{ $sponsorship->id }}</th>
+                                <td>{{ $sponsorship->flat_id }}</td>
+                                <td>{{ $sponsorship->sponsor_type }}</th>
+                                <td>{{ Carbon\Carbon::parse($sponsorship->start)->format('d/m/Y h:m') }}</td>
+                                <td>{{ Carbon\Carbon::parse($sponsorship->end)->format('d/m/Y h:m') }}</td>
+                                <td>{{ $sponsorship->price }}</td>
+                                <td>{{ $sponsorship->braintree_code }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                <div class="active-legend-div-indexSponsorship-page">
+                    <div class="greendiv-indexSponsorship-page"><i class="fas fa-circle"></i></div>
+                    <span class="span-indexSponsorship-page">= la sponsorizzazione è attiva</span>
+                </div>
             </div>
-        </div>
+        @else 
+            <h1 class="empty-h1-sponsorship-page p-50">
+                Non hai ancora nessun appartamento sponsorizzato.
+            </h1>
+        @endif
     </div>
 @endsection
