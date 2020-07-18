36b0c25a45654e19b20fb98bedecf5cb021b289a
Riccardo Murru <riccardo.mr@gmail.com>
2020-07-18 20:01:02 +0200

    add. form css

--- resources/views/shared/components/formCreate.blade.php
+++ resources/views/shared/components/formCreate.blade.php
@@ -1,75 +1,50 @@
-<div class="edit-form df-column align-center">
-    <form class="df-column" action="{{ route('admin.flats.store') }}" method="POST" enctype="multipart/form-data">
+<div class="df-column align-center">
+    <form class="df-column form mb-20" action="{{ route('admin.flats.store') }}" method="POST"
+        enctype="multipart/form-data">
         @csrf
         @method('POST')
-        <div class="title df-column align-center">
-            <label for="title">Titolo</label>
-            <input type="text" name="title" id="title" value="{{ old('title') }}">
-        </div>
-
-        <div class="edit-description">
-            <div class="desc-one df-column align-center">
-                <label for="description">Descrizione</label>
-                <textarea name="description" id="description" cols="30" rows="10">{{ old('description') }}</textarea>
-            </div>
-
-            <div class="desc-two df-column align-center">
-                <div class="d-flex s-center align-center">
-                    <label for="number_of_rooms">Numero di stanze</label>
-                    <input 
-                        type="number" 
-                        name="number_of_rooms" 
-                        id="number_of_rooms" 
-                        value="{{ old('number_of_rooms') }}"
-                        min="1"
-                    >
-                    <label for="number_of_beds">Numero di letti</label>
-                    <input 
-                        type="number" 
-                        name="number_of_beds" 
-                        id="number_of_beds" 
-                        value="{{ old('number_of_beds') }}"
-                        min="1"
-                    >
+        <ul>
+            <li>
+                <input type="text" name="title" class="field-style field-full" placeholder="Titolo"
+                    value="{{ old('title')}}" />
+            </li>
+            <li>
+                <textarea name="description" class="field-style"
+                    placeholder="Descrizione">{{ old('description') }}</textarea>
+            </li>
+            <li class="d-flex s-between">
+                <input type="number" name="number_of_rooms" class="field-style field-split align-left" min="1"
+                    placeholder="Stanze" value="{{ old('number_of_rooms') }}" />
+                <input type="number" name="number_of_beds" class="field-style field-split align-left" min="1"
+                    placeholder="Letti" value="{{ old('number_of_beds') }}" />
+            </li>
+            <li class="d-flex s-between">
+                <input type="number" name="number_of_bathrooms" class="field-style field-split align-left" min="1"
+                    placeholder="Bagni" value="{{ old('number_of_bathrooms') }}" />
+                <input type="number" name="nsquare_meters" class="field-style field-split align-left" min="10"
+                    placeholder="Metri quadri" value="{{ old('square_meters') }}" />
+            </li>
+            <li class="inputAlgolia-page">
+                <input type="text" name="address" id="address" placeholder="Indirizzo">
+                <input type="hidden" name="latlong" id="inputAlgolia-search__latlong">
+            </li>
+            <li>
+                <label for="image">Immagine</label>
+                <input class="field-style field-full" type="file" name="image" id="image" accept="image/*"
+                    value="{{ old('image') }}">
+            </li>
+            <li class="services text-center">
+                @foreach ($services as $service)
+                <div class="form-group d-flex">
+                    <input class="mr-10 ml-10" type="checkbox" name="services[]" id="service-{{ $loop->iteration }}"
+                        value="{{ $service->id }}">
+                    <label for="service-{{ $loop->iteration }}">{{ $service->type }}</label>
                 </div>
-                
-                <div class="d-flex s-center align-center">
-                    <label for="number_of_bathrooms">Numero di bagni</label>
-                    <input 
-                        type="number" 
-                        name="number_of_bathrooms" 
-                        id="number_of_bathrooms" 
-                        value="{{ old('number_of_bathrooms') }}"
-                        min="1"
-                    >
-                    <label for="square_meters">Metri quadri</label>
-                    <input 
-                        type="number" 
-                        name="square_meters" 
-                        id="square_meters" 
-                        value="{{ old('square_meters') }}"
-                        min="1"
-                    >
-                </div>
-            </div>
-        </div>
-
-        <div class="address df-column align-center">
-            <label for="address">Indirizzo</label>
-            @include('shared.components.inputAlgolia')
-        </div>
-
-        <div class="add-image df-column align-center">
-            <label for="image">Immagine</label>
-            <input type="file" name="image" id="image" accept="image/*" value="{{ old('image') }}">
-        </div>
-
-        <div class="services df-column align-center">
-            @foreach ($services as $service)
-                <input type="checkbox" name="services[]" id="service-{{ $loop->iteration }}" value="{{ $service->id }}">
-                <label for="ag-{{ $loop->iteration }}">{{ $service->type }}</label>
-            @endforeach
-        </div>
-        <input type="submit" value="Aggiungi" class="btn-search">
+                @endforeach
+            </li>
+            <li>
+                <input class="btn-search" type="submit" value="Aggiungi appartamento" />
+            </li>
+        </ul>
     </form>
 </div>
