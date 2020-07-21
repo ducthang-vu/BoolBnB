57c88a7c4e74e5ac2d9a083c9c20fe628bc5aecf
Duc Thang Vu <vuducthang91@gmail.com>
2020-07-21 12:18:56 +0200

    fix

--- resources/js/app.js
+++ resources/js/app.js
@@ -83,47 +83,6 @@ if (document.querySelector(".card-show")) {
     mapShow(lat, lng, iconPath);
 }
 
-try {
-    const canvasVisualisations = document.getElementById(
-        "chart-visualisations"
-    );
-    const canvasNumberOfRequests = document.getElementById(
-        "chart-numberOfRequests"
-    );
-
-    const chartVisualisations = new Chart(canvasVisualisations, {
-        type: "bar",
-        data: {
-            datasets: [
-                {
-                    //barPercentage: 0.5,
-                    //barThickness: 6,
-                    //maxBarThickness: 8,
-                    //minBarLength: 2,
-                    data: [visualisations],
-                    labels: ["visualisations"]
-                }
-            ]
-        }
-    });
-
-    const chartNumberOfRequests = new Chart(canvasNumberOfRequests, {
-        type: "bar",
-        data: {
-            datasets: [
-                {
-                    //barPercentage: 0.5,
-                    //barThickness: 6,
-                    //maxBarThickness: 8,
-                    //minBarLength: 2,
-                    data: [numberOrRequests],
-                    labels: ["number or requests"]
-                }
-            ]
-        }
-    });
-} catch {} // do nothing
-
 /***********
  * Functions
  **********/
