223e2fcfcc817aa6e531a63a90fd1073ef72aba4
Nicola <niconix.dev@gmail.com>
2020-07-21 11:44:16 +0200

    fix message error & success

--- resources/views/guest/flats/show.blade.php
+++ resources/views/guest/flats/show.blade.php
@@ -3,12 +3,12 @@
 
 @section('page-content')
     @if(session('created'))
-        <div class="alert alert-success transition-invisible">
+        <div class="alert alert-success transition-invisible success-message">
             <p>Your message have been sent to the host!</p>
         </div>
     @endif
     @if ($errors->any())
-        <div class="alert alert-danger">
+        <div class="alert alert-danger error-message">
             <ul>
                 @foreach ($errors->all() as $error)
                     <li>{{ $error }}</li>
