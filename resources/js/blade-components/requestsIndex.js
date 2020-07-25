d52d43ba9dc2229074dc0fbbcbb533f84cf594b3
Riccardo Murru <riccardo.mr@gmail.com>
2020-07-25 19:00:33 +0200

    various fixes

--- resources/js/blade-components/requestsIndex.js
+++ resources/js/blade-components/requestsIndex.js
@@ -10,19 +10,5 @@ function showRequestMessage() {
         });
     });
 }
-// function showRequestMessage() {
-//     let messageHandler = document.querySelectorAll(".request-message-handler");
-
-//     messageHandler.forEach(element => {
-//         element.addEventListener("click", function() {
-//             let message = this.querySelector(".request-message");
-//             let controls = this.querySelectorAll("h4");
-//             controls.forEach(element => {
-//                 element.classList.toggle("active");
-//             });
-//             message.classList.toggle("active");
-//         });
-//     });
-// }
 
 export default showRequestMessage;
--- resources/views/admin/requests/index.blade.php
+++ resources/views/admin/requests/index.blade.php
@@ -22,24 +22,6 @@
         </ul>
         <p class="l-height-150 no-display message-text pt-25">{{ $request->message }}</p>
     </div>
-    {{-- <div class="request-card d-flex p-20">
-        <div class="request-details">
-            <h3>ID Richiesta: {{ $request->id }}</h3>
-    <h4>ID Appartamento: {{ $request->flat_id }}</h4>
-    <h4>Ricevuta da:
-        <span class="request-text">{{ $request->name }} {{ $request->surname }}</span>
-    </h4>
-    <h4>Ricevuto il: <span
-            class="request-text">{{ Carbon\Carbon::parse($request->created_at)->format('d/m/Y h:m') }}</span>
-    </h4>
-    <h4>Email: <span class="request-text">{{ $request->email }}</span></h4>
-</div>
-<div class="request-message-handler">
-    <h4 class="message-control active text-center">Mostra Messaggio:</h4>
-    <h4 class="message-control text-center">Nascondi Messaggio</h4>
-    <p class="request-message">{{ $request->message }}</p>
-</div>
-</div> --}}
-@endforeach
+    @endforeach
 </div>
 @endsection
