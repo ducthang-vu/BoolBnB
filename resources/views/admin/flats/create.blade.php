c97aaee9c8022ea1197c9e0c23650cea7aaf1e55
Duc Thang Vu <vuducthang91@gmail.com>
2020-07-25 22:59:42 +0200

    fix

--- resources/views/admin/flats/create.blade.php
+++ resources/views/admin/flats/create.blade.php
@@ -1,24 +1,24 @@
 @extends('layouts.main')
 
 @section('page-content')
-<div class="section-home-admin mt-20">
-    <div class="title-flts text-center">
-        <h1 class="mt-20 mb-10">
-            Aggiungi un appartamento
-        </h1>
-    </div>
-
     @if ($errors->any())
-    <div class="alert alert-danger error-message">
-        <ul>
-            @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
+        <div class="error-message message-animation">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li><h2>{{ $error }}</h2></li>
+                @endforeach
+            </ul>
+        </div>
     @endif
 
-    @include('shared.components.formCreate')
+    <div class="section-home-admin mt-20">
+        <div class="title-flts text-center">
+            <h1 class="mt-20 mb-10">
+                Aggiungi un appartamento
+            </h1>
+        </div>
 
-</div>
+        @include('shared.components.formCreate')
+
+    </div>
 @endsection
--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,33 +1,28 @@
 @extends('layouts.main')
 
 @section('page-content')
+    <div class="section-home">
+        <div class="pic-card d-flex s-center">
+            <img src="https://i.ibb.co/64Pxb9p/maxresdefault.jpg" alt="maxresdefault">
+            @include('shared.components.formAlgoliaHome')
+        </div>
 
-<div class="section-home">
-    <div class="pic-card d-flex s-center">
-        <img src="https://i.ibb.co/64Pxb9p/maxresdefault.jpg" alt="maxresdefault">
-        @include('shared.components.formAlgoliaHome')
+        <div class="sponsorship-card d-flex s-center">
+            @foreach($sponsoredFlats as $sponsoredFlat)
+                <a class="card" href="{{ route('flats.show' , $sponsoredFlat->id) }}">
+                    <div class="image-dev">
+                        <img src="{{ asset('storage/' . $sponsoredFlat->image ) }}" alt="{{$sponsoredFlat->title}}">
+                    </div>
+
+                    <div class="text-card">
+                        <h5>{{$sponsoredFlat->title}}</h5>
+                        <p>{{$sponsoredFlat->address}}</p>
+                    </div>
+                </a>
+                @if ($loop->index == 5)
+                    @break;
+                @endif
+            @endforeach
+        </div>
     </div>
-
-
-    <div class="sponsorship-card d-flex s-center">
-        @foreach($sponsoredFlats as $sponsoredFlat)
-
-        <a class="card" href="{{ route('flats.show' , $sponsoredFlat->id) }}">
-            <div class="image-dev">
-                <img src="{{ asset('storage/' . $sponsoredFlat->image ) }}" alt="{{$sponsoredFlat->title}}">
-            </div>
-            
-            <div class="text-card">
-                <h5>{{$sponsoredFlat->title}}</h5>
-                <p>{{$sponsoredFlat->address}}</p>
-            </div>
-        </a>
-
-        @if ($loop->index == 5)
-        @break;
-        @endif
-        @endforeach
-    </div>
-</div>
-
 @endsection
