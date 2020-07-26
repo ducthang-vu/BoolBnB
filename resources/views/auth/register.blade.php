8c976e7c4cb99d8f84c144287f095e431897f378
Riccardo Murru <riccardo.mr@gmail.com>
2020-07-26 15:26:35 +0200

    fix login register errors

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,8 +1,9 @@
 <div class="container-register">
     <div class="card-popup">
-    
+
         <div class="card-header">{{ __('Registrati') }}
-            <span onclick="closePopUpRegister()" class="close-popup-page popup-animation"><i class="far fa-times-circle"></i></span>
+            <span onclick="closePopUpRegister()" class="close-popup-page popup-animation"><i
+                    class="far fa-times-circle"></i></span>
         </div>
 
         <div class="card-body">
@@ -10,62 +11,73 @@
                 @csrf
 
                 <div class="div-register-page pb-10 pt-20">
-                    <input id="name" type="text" class="form-control-register-page @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" placeholder="NAME" autofocus>
+                    <input id="name" type="text" class="form-control-register-page @error('name') is-invalid @enderror"
+                        name="name" value="{{ old('name') }}" required autocomplete="name" placeholder="NAME" autofocus>
 
                     @error('name')
-                        <span class="invalid-feedback-register-page block" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
+                    <span class="invalid-feedback-register-page block" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
                     @enderror
                 </div>
 
 
                 <div class="div-register-page pb-10">
-                    <input id="surname" type="text" class="form-control-register-page @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" required autocomplete="surname" placeholder="SURNAME" autofocus>
+                    <input id="surname" type="text"
+                        class="form-control-register-page @error('surname') is-invalid @enderror" name="surname"
+                        value="{{ old('surname') }}" required autocomplete="surname" placeholder="SURNAME" autofocus>
 
                     @error('surname')
-                        <span class="invalid-feedback-register-page block" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
+                    <span class="invalid-feedback-register-page block" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
                     @enderror
                 </div>
 
 
                 <Div class="div-register-page pb-10">
-                    <input id="date_of_birth" type="text" class="form-control-register-page @error('date_of_birth') is-invalid @enderror" name="date_of_birth" value="{{ old('date_of_birth') }}" required autocomplete="date_of_birth" placeholder="DATE OF BIRTHDAY YYYY-MM-DD" autofocus>
+                    <input id="date_of_birth" type="text"
+                        class="form-control-register-page @error('date_of_birth') is-invalid @enderror"
+                        name="date_of_birth" value="{{ old('date_of_birth') }}" required autocomplete="date_of_birth"
+                        placeholder="DATE OF BIRTHDAY YYYY-MM-DD" autofocus>
 
                     @error('date_of_birth')
-                        <span class="invalid-feedback-register-page block" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
+                    <span class="invalid-feedback-register-page block" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
                     @enderror
                 </div>
 
 
                 <div class="div-register-page pb-10">
-                    <input id="email" type=""email" class="form-control-register-page @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="E-MAIL" required autocomplete="email">
+                    <input id="email" type="email"
+                        class="form-control-register-page @error('email') is-invalid @enderror" name="email"
+                        value="{{ old('email') }}" placeholder="E-MAIL" required autocomplete="email">
 
-                    @error('"email')
-                        <span class="invalid-feedback-register-page block" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
+                    @error("email")
+                    <span class="invalid-feedback-register-page block" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
                     @enderror
                 </div>
 
 
                 <div class="div-register-page pb-10">
-                    <input id="password" type="password" class="form-control-register-page @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="PASSWORD">
+                    <input id="password" type="password"
+                        class="form-control-register-page @error('password') is-invalid @enderror" name="password"
+                        required autocomplete="new-password" placeholder="PASSWORD">
 
                     @error('password')
-                        <span class="invalid-feedback block" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
+                    <span class="invalid-feedback block" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
                     @enderror
                 </div>
 
 
                 <div class="div-register-page pb-10">
-                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="CONFIRM PASSWORD">
+                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation"
+                        required autocomplete="new-password" placeholder="CONFIRM PASSWORD">
                 </div>
 
                 <div class="div-register-page pb-10">
@@ -78,4 +90,3 @@
         </div>
     </div>
 </div>
-
--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,6 +12,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
 </head>
+
 <body>
     <div class="popup-wrapper-header">
     </div>
