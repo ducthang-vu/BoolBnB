02a9774ad583c52e44c501befd257b7c4affc7de
Riccardo Murru <riccardo.mr@gmail.com>
2020-07-26 17:09:38 +0200

    fix link in sponsorship table

--- resources/views/admin/sponsorships/components/tableSponsorship.blade.php
+++ resources/views/admin/sponsorships/components/tableSponsorship.blade.php
@@ -12,22 +12,21 @@
     </thead>
     <tbody>
         @foreach($sponsorships as $sponsorship)
-            <tr
-                class="
+        <tr class="
                     @if (Carbon\Carbon::parse($sponsorship->end)
                             ->greaterThanOrEqualTo(Carbon\Carbon::now()))
                         active
                     @endif
-                    "
-            >
-                <td scope="row" data-label="Codice">{{ $sponsorship->id }}</th>
-                <td data-label="Appartamento">{{ $sponsorship->flat_id }}</td>
-                <td data-label="Tipo">{{ $sponsorship->sponsor_type }}</th>
-                <td data-label="Inizio">{{ Carbon\Carbon::parse($sponsorship->start)->format('d/m/Y h:m') }}</td>
-                <td data-label="Fine">{{ Carbon\Carbon::parse($sponsorship->end)->format('d/m/Y h:m') }}</td>
-                <td data-label="Prezzo">€ {{ $sponsorship->price }}</td>
-                <td data-label="Codice pagamento">{{ $sponsorship->braintree_code }}</td>
-            </tr>
+                    ">
+            <td scope="row" data-label="Codice">{{ $sponsorship->id }}</th>
+            <td data-label="Appartamento"><a
+                    href="{{ route('admin.flats.show', $sponsorship->flat_id) }}">{{ $sponsorship->flat_id }}</a></td>
+            <td data-label="Tipo">{{ $sponsorship->sponsor_type }}</th>
+            <td data-label="Inizio">{{ Carbon\Carbon::parse($sponsorship->start)->format('d/m/Y h:m') }}</td>
+            <td data-label="Fine">{{ Carbon\Carbon::parse($sponsorship->end)->format('d/m/Y h:m') }}</td>
+            <td data-label="Prezzo">€ {{ $sponsorship->price }}</td>
+            <td data-label="Codice pagamento">{{ $sponsorship->braintree_code }}</td>
+        </tr>
         @endforeach
     </tbody>
 </table>
